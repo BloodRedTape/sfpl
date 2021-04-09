3a4473c0b2947e7335c3d83caf9981079494e2bb
E1Hephaestus <gefestroyale@gmail.com>
2021-04-09 10:43:09 +0300

    [0.2]: Automatic array size deduction

--- sources/libplot.hpp
+++ sources/libplot.hpp
@@ -15,12 +15,20 @@ struct TraceData{
 struct PlotBuilder{
     static bool Trace(const char *outfilepath, const char *title, size_t image_width, size_t image_height, const TraceData traces[], size_t traces_count);
 
+    template<size_t N>
+    static bool Trace(const char *outfilepath, const char *title, size_t image_width, size_t image_height, const TraceData (&traces)[N]);
+
     static bool Trace(const char *outfilepath, const char *title, size_t image_width, size_t image_height, const TraceData &trace);
 
 };
 
 inline bool PlotBuilder::Trace(const char *outfilepath, const char *title, size_t image_width, size_t image_height, const TraceData &trace){
     return Trace(outfilepath, title, image_width, image_height, &trace, 1);
+}
+
+template<size_t N>
+inline bool PlotBuilder::Trace(const char *outfilepath, const char *title, size_t image_width, size_t image_height, const TraceData (&traces)[N]){
+    return Trace(outfilepath, title, image_width, image_height, traces, N);
 }
 
 }//namespace libplot::
